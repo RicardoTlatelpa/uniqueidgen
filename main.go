4cbed3f06f96162adf116adc8db88577c3c8fe5d
Ricardo <ricardo@Ricardos-MacBook-Pro.local>
2025-07-08 00:13:55 -0400

    add generator class to main file and print out uniquely generated ids

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+	
+	"uniqueidgen/gen"
+)
+
+func main() {
+	// create a new generator with worker ID 1
+
+	g, err := gen.NewGen(1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		id, err := g.NextID()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(id)
+		time.Sleep(1 * time.Millisecond)
+	}
+}
